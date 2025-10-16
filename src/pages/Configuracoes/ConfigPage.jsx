24bf59196ce4e92eb461b3a36c2a61f24ac3788e
Pablo Vinícios <pablo.araujo@alu.ufc.br>
2025-10-16 00:28:44 -0300

    feat: adding new components from mui material

--- src/pages/Configuracoes/ConfigPage.jsx
+++ src/pages/Configuracoes/ConfigPage.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Box, Typography, Avatar, IconButton, Button } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
+import EditIcon from '@mui/icons-material/Edit';
+import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 
 export default function ConfiguracoesPage() {
     return (
