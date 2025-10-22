9332d7125c2f9eda701309201fab78b4c24db095
Pablo Vinícios <pablo.araujo@alu.ufc.br>
2025-10-22 11:02:49 -0300

    feat: create admin table

--- src/pages/Configuracoes/ConfigPage.jsx
+++ src/pages/Configuracoes/ConfigPage.jsx
@@ -50,3 +50,70 @@ const mockFuncionarios = [
   { id: 4, nome: "Victor", matricula: "192845", email: "victor@email.com" },
   { id: 5, nome: "Oliveira", matricula: "172839", email: "oliveira@email.com" },
 ];
+
+function AdminArea({ funcionarios, onAddUser, onEditUser, onDeleteUser }) {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+    return (
+    <Box>
+      <Typography variant="h4" fontWeight="bold" mb={3}>Área do Administrador</Typography>
+      <Button
+        variant="contained"
+        startIcon={<Add />}
+        sx={{ ...yellowButtonSx, mb: 3 }}
+        onClick={onAddUser}
+      >
+        Cadastrar Novos Usuários
+      </Button>
+
+      <TableContainer component={Paper} elevation={3} sx={{ borderRadius: 2 }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ fontWeight: 'bold' }}>Nome</TableCell>
+              <TableCell sx={{ fontWeight: 'bold' }}>Matrícula</TableCell>
+              <TableCell sx={{ fontWeight: 'bold' }}>Ação</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {funcionarios
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((user) => (
+                <TableRow key={user.id}>
+                  <TableCell>{user.nome}</TableCell>
+                  <TableCell>{user.matricula}</TableCell>
+                  <TableCell>
+                    <IconButton onClick={() => onEditUser(user)}>
+                      <Edit />
+                    </IconButton>
+                    <IconButton onClick={() => onDeleteUser(user)}>
+                      <Delete />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={funcionarios.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          labelRowsPerPage="Rows per page:"
+        />
+      </TableContainer>
+    </Box>
+  );
+}
