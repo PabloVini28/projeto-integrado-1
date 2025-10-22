e0b7fdb39b0fb013973c97df49c3c7162340b8a0
Pablo Vinícios <pablo.araujo@alu.ufc.br>
2025-10-22 11:00:34 -0300

    feat: try to simulate, token jwt work

--- src/pages/Configuracoes/ConfigPage.jsx
+++ src/pages/Configuracoes/ConfigPage.jsx
@@ -28,3 +28,25 @@ const grayButtonSx = {
     bgcolor: '#4B5563',
   },
 };
+
+const mockUserAdmin = {
+  nome: "Kelton Martins",
+  matricula: "123456",
+  email: "kelton@admin.com",
+  role: "ADMINISTRADOR"
+};
+
+const mockUserFuncionario = {
+  nome: "Julio Mateus Morais",
+  matricula: "654321",
+  email: "julio@funcionario.com",
+  role: "FUNCIONARIO"
+};
+
+const mockFuncionarios = [
+  { id: 1, nome: "Julio Mateus Morais", matricula: "123456", email: "julio@email.com" },
+  { id: 2, nome: "Pablo", matricula: "147897", email: "pablo@email.com" },
+  { id: 3, nome: "Guilherme pinheiro", matricula: "123783", email: "gui@email.com" },
+  { id: 4, nome: "Victor", matricula: "192845", email: "victor@email.com" },
+  { id: 5, nome: "Oliveira", matricula: "172839", email: "oliveira@email.com" },
+];
