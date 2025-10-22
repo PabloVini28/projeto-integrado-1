45ee166796e7ab08b1117c7fff10e2e34c01d9fd
Pablo Vinícios <pablo.araujo@alu.ufc.br>
2025-10-22 10:56:20 -0300

    feat: importing essential components from mui

--- src/pages/Configuracoes/ConfigPage.jsx
+++ src/pages/Configuracoes/ConfigPage.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import { Typography } from '@mui/material';
 
-export default function ConfiguracoesPage() {
-    return (
-        <Typography variant="h4">
-            Página de Configurações
-        </Typography>
-    );
-}
+import React, { useState, useEffect } from 'react';
+import {
+  Box, Typography, Paper, Grid, Button, Dialog, DialogTitle, DialogContent,
+  DialogActions, TextField, Table, TableBody, TableCell,
+  TableContainer, TableHead, TableRow, TablePagination, IconButton,
+  Breadcrumbs, Link
+} from '@mui/material';
+import {
+  PersonOutline, DescriptionOutlined, AdminPanelSettingsOutlined,
+  ChevronRight, Add, Edit, Delete, NavigateNext // <-- Icone para o Breadcrumb
+} from '@mui/icons-material';
+
