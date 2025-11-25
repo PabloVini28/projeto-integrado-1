112d866d223e48edf01a4f3ca754b4c79b46e616
Pablo Vinícios <pablo.araujo@alu.ufc.br>
2025-11-24 21:51:16 -0300

    feat: add sequence for funcionario ID and adjust primary key definition

--- backend/src/migrations/004_create_funcionario.sql
+++ backend/src/migrations/004_create_funcionario.sql
@@ -1,10 +1,12 @@
+CREATE SEQUENCE IF NOT EXISTS funcionarios_id_seq;
+
 CREATE TABLE IF NOT EXISTS funcionarios (
-	id_funcionario VARCHAR(64) PRIMARY KEY,
+	id_funcionario VARCHAR(64) DEFAULT nextval('funcionarios_id_seq')::text PRIMARY KEY,
 	nome_funcionario VARCHAR(255) NOT NULL,
 	email_funcionario VARCHAR(255) UNIQUE NOT NULL,
 	cpf_funcionario VARCHAR(14) UNIQUE NOT NULL,
-    senha VARCHAR(255) NOT NULL,
-    nivel_acesso VARCHAR(64) NOT NULL DEFAULT 'Funcionario',
+	senha VARCHAR(255) NOT NULL,
+	nivel_acesso VARCHAR(64) NOT NULL DEFAULT 'Funcionario',
 	isEnabled BOOLEAN NOT NULL DEFAULT FALSE,
 	verificationCode VARCHAR(128),
 	verificationCodeExpiry TIMESTAMP WITH TIME ZONE,
